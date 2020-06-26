ba62d63dbc0c23dd06775e299e5dba335115d81b
Grégoire Hertault <ghertault@gmail.com>
2020-06-26 10:35:11 +0200

    Include Amplify in peer deps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import { Amplify } from "@aws-amplify/core";
 import { AmplifyAdmin, AmplifyFilter } from "./components";
 import { buildAuthProvider, buildDataProvider } from "./providers";
-
-export function configureAmplify(config: Record<string, unknown>): void {
-  Amplify.configure(config);
-}
 
 export { AmplifyAdmin, AmplifyFilter, buildAuthProvider, buildDataProvider };
