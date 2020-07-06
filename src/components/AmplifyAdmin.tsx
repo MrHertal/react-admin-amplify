4922d05f0a93d80a672a2fc30721d4e50a6c7881
Grégoire Hertault <ghertault@gmail.com>
2020-07-06 10:21:19 +0200

    Simplify AmplifyAdmin component (#7)

--- src/components/AmplifyAdmin.tsx
+++ src/components/AmplifyAdmin.tsx
@@ -1,5 +1,4 @@
-import { AuthProvider, DataProvider } from "ra-core";
-import React, { useEffect, useState } from "react";
+import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Admin } from "react-admin";
 import { buildAuthProvider, buildDataProvider } from "../providers";
@@ -17,33 +16,14 @@ export const AmplifyAdmin: React.FC<{
   operations: Operations;
   options?: AmplifyAdminOptions;
 }> = ({ children, operations, options = defaultOptions, ...propsRest }) => {
-  const [authProvider, setAuthProvider] = useState<AuthProvider | null>(null);
-  const [dataProvider, setDataProvider] = useState<DataProvider | null>(null);
-
   const optionsBag = { ...defaultOptions, ...options };
   const authGroups = optionsBag.authGroups as string[];
-
-  useEffect(() => {
-    const provider = buildAuthProvider({ authGroups });
-
-    setAuthProvider(() => provider);
-  }, []);
-
-  useEffect(() => {
-    const provider = buildDataProvider(operations);
-
-    setDataProvider(() => provider);
-  }, []);
-
-  if (!authProvider || !dataProvider) {
-    return <div>Loading</div>;
-  }
 
   return (
     <Admin
       {...propsRest}
-      authProvider={authProvider}
-      dataProvider={dataProvider}
+      authProvider={buildAuthProvider({ authGroups })}
+      dataProvider={buildDataProvider(operations)}
     >
       {children}
     </Admin>
